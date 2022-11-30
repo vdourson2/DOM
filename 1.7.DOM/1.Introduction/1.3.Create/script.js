136f14ac36fe4aa3f066bcd6b67e47e497fb6d25
Virginie Dourson <virginiedourson@gmail.com>
2022-11-30 11:36:43 +0100

    Exercice 2 du 1.3

--- 1.7.DOM/1.Introduction/1.3.Create/script.js
+++ 1.7.DOM/1.Introduction/1.3.Create/script.js
@@ -15,22 +15,25 @@ for (let i=0;i<learners.length;i++){
 
     //Application de la couleur au fond de la div
     divel.style.backgroundColor=`rgb(${r},${g},${b})`;
-    
-    //console.log(`rgb(${r},${g},${b})`);
-    //let light = lightness("hsl(90, 100%, 50%)");
-    //console.log(light);
-    //if (lightness()`rgb(${r},${g},${b})`)
-    
+        
     //Création du noeud p et du noeud texte
     let paragr = document.createElement("p");
     let nom = document.createTextNode(learners[i]);
-    
-    
+        
     //Extraction de la luminosité de la couleur rgb
+    //et changement de la couleur du texte si la luminosité est 
+    //inférieure à 60%
     let M = Math.max(r,g,b);
     let m = Math.min(r,g,b);
-    let L = (M+m)/510;
+    let L = ((M+m)/510)*100;
+    console.log(r + " " + g + " " + b);
+    console.log(L)
+    console.log(nom)
+    if (L<60){
+        paragr.style.color="rgb(255,255,255)";
+    }
 
+    //Rattachement des différents noeuds à leur parent
     paragr.appendChild(nom);
     divel.appendChild(paragr);
     emplacement.appendChild(divel);
